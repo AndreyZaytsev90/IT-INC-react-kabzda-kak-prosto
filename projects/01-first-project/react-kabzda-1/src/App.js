cd0e6af9680e9c3d4c0dff2a0bd65465803442db
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-12-24 20:55:10 +0300

    added new project

---
+++ projects/01-first-project/react-kabzda-1/src/App.js
@@ -0,0 +1,19 @@
+//import './logo.svg';
+import './App.css';
+import Technologies from "./Technologies.js";
+import Header from "./Header.js";
+
+
+const App = () => {
+    return (
+        <div>
+            <Header />
+            <Technologies />
+        </div>
+    )
+}
+
+
+
+
+export default App;
